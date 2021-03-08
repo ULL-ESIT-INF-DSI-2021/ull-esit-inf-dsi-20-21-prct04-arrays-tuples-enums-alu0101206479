12944bf9a8cee5869ed21a33cdfd023368b71458
ACOIDAN MESA HERNANDEZ <alu0101026479@ull.edu.es>
2021-03-08 15:52:03 +0000

    Estructura inicial y documentación creada

---
+++ src/ejercicio-1.ts
@@ -0,0 +1,13 @@
+/**
+ * ```typescript
+ *  console.log(muestraTexto("Hola mundo"));
+ * ```
+ * _Función_ que devuelve un __texto de entrada__
+ * @param texto Texto de entrada
+ * @return Devuelve el texto
+ */
+function muestraTexto(texto: string): string {
+  return texto;
+}
+
+console.log(muestraTexto("Hola mundo"));
