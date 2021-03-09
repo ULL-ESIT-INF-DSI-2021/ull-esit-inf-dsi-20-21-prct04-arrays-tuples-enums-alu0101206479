fd4b9d16ff770f7033648ed14fc56bc1b5ccd3bc
ACOIDAN MESA HERNANDEZ <alu0101026479@ull.edu.es>
2021-03-09 18:19:25 +0000

    ¡ERROR! Expectativa sin código (Ejercicio 1)

--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -1,9 +1,13 @@
 import 'mocha';
 import {expect} from 'chai';
-import {muestraTexto} from '../src/ejercicio-1';
+import {decodeResistor} from '../src/ejercicio-1';
 
 describe('Prueba de ejercicio 1', () => {
-  it('Muestra texto "Hola mundo"', () => {
-    expect(muestraTexto("Hola mundo")).to.be.equal("Hola mundo");
+  it('Valor de una resistencia con dos colores "decodeResistor(["Marrón", "Verde"])"', () => {
+    expect(decodeResistor(["Marrón", "Verde"])).to.be.equal(15);
   });
+
+  it('Valor de una resistencia con tres colores "decodeResistor(["Marrón", "Verde"])"'), () => {
+    expect(decodeResistor(["Marrón", "Verde", "Violeta"])).to.be.equal(15);
+  };
 });
