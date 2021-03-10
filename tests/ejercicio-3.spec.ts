eae4c046b78547c9088cc83d6ecff10e582d7e40
ACOIDAN MESA HERNANDEZ <alu0101026479@ull.edu.es>
2021-03-10 10:52:09 +0000

    ¡FALLO! Sólo expectativa sin código (Ejercicio 3)

---
+++ tests/ejercicio-3.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+import {meanAndConcatenate} from '../src/ejercicio-3';
+
+describe('Prueba de ejercicio 3', () => {
+  it('meanAndConcatenate(['u', 6, 'd', 1, 'i', 'w', 6, 's', 't', 4, 'a', 6, 'g', 1, 2, 'w', 8, 'o', 2, 0]) returns array [3.6, "udiwstagwo"]', () => {
+    expect(meanAndConcatenate(['u', 6, 'd', 1, 'i', 'w', 6, 's', 't', 4, 'a', 6, 'g', 1, 2, 'w', 8, 'o', 2, 0])).to.be.equal([3.6, "udiwstagwo"]);
+  });
+});
